9185bb160c4d5176f863e07b1489452d186f37bc
Manohar Chaudhari <manohar.chaudhari@globatn.com>
2019-09-03 18:41:53 +0530

    added promise advanced puzzle

---
+++ promise-advanced.js
@@ -0,0 +1,36 @@
+//https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html
+function doSomething() {
+  return Promise.resolve(10);
+}
+function doSomethingElse() {
+  return Promise.resolve(20);
+}
+
+//1
+doSomething()
+  .then(function() {
+    return doSomethingElse();
+  })
+  .then(function(result) {
+    console.log(result); // will return 20
+  });
+//2
+doSomething()
+  .then(function() {
+    doSomethingElse();
+  })
+  .then(function(result) {
+    console.log(result); // will return undefined
+  });
+// 3
+doSomething()
+  .then(doSomethingElse())
+  .then(function(result) {
+    console.log(result); // will return 10
+  });
+//4.
+doSomething()
+  .then(doSomethingElse)
+  .then(function(result) {
+    console.log(result); //will return 20
+  });
